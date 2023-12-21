f244f945d88b0f4b2bbfc86cf8ccd6125f2c7725
Nicolas Ramirez <113265532+Muertoparado@users.noreply.github.com>
2023-12-20 19:34:17 -0500

    feat: :bug: tamano y render circuito solved

--- Portafolio/src/components/3d/spline.jsx
+++ Portafolio/src/components/3d/spline.jsx
@@ -1,15 +1,53 @@
+/*
 import Spline from '@splinetool/react-spline';
 import Container from '@mui/material/Container';
 import Box from '@mui/material/Box';
+//import SplineOptimized from './splineopti.jsx';
 
 export default function Circuit() {
   return (
-    <Box>
-      <Container>
-      <Spline scene="https://prod.spline.design/JbZeqcYK7gvFEGaN/scene.splinecode" />
-      </Container>
-    </Box>
-   
+    
+      <Box className='size' >
+        <Spline scene="https://prod.spline.design/JbZeqcYK7gvFEGaN/scene.splinecode"   />
+      </Box>
+    
   );
-}
+} */
+  /*
+  return (
+    <Container>
+      <SplineOptimized />
+    </Container>
+  )*/
+
+  import React, { lazy, Suspense } from 'react';
+import Container from '@mui/material/Container';
+import Box from '@mui/material/Box';
+
+// Utilizando React.lazy para cargar el componente de forma perezosa
+const LazySpline = lazy(() => import('@splinetool/react-spline'));
+
+// Manejando el error de carga del modelo
+const ErrorFallback = () => (
+  <div>
+    <p>Ocurrió un error al cargar el modelo 3D.</p>
+  </div>
+);
+
+const Circuit = () => {
+  return (
+    <Container>
+      {/* Suspense para mostrar un fallback mientras se carga el componente */}
+      <Suspense fallback={<div>Loading...</div>} >
+        <Box className='size'>
+          {/* Renderiza el componente 3D solo cuando es necesario */}
+          <LazySpline scene="https://prod.spline.design/JbZeqcYK7gvFEGaN/scene.splinecode" />
+        </Box>
+      </Suspense>
+    </Container>
+  );
+};
+
+export default Circuit;
+
 
--- Portafolio/src/components/home/home.jsx
+++ Portafolio/src/components/home/home.jsx
@@ -5,7 +5,7 @@ import Presentacion from "../presentacion/presentacion.jsx";
 import Container from '@mui/material/Container';
 import Grid from '@mui/material/Grid';
 import './home.css';
-import NFTCard from "../3d/cards/cards.jsx";
+import NFTCard from "../cards/cards.jsx";
 function Home() {
   return (
     <Box>
@@ -13,26 +13,32 @@ function Home() {
       <Navbar />
       <Presentacion />
       </Box>
-      <Container className="border-top" maxWidth="lg">
+      <Container className="border-top" maxWidth="lg" >
       <h1>
         About Me
       </h1>
-      <Grid container alignItems="stretch" style={{height: '100%'}}>
 
-    <Grid item xs={6} >
-    <Circuit className="margin-top" />
-              <p>*Al presionar los pulsadores se enciende el led*</p>
-    </Grid>
+      <Grid container alignItems="stretch" spacing={2}>
 
-    <Grid item xs={6}>
-      <p style={{height: '100%'}}> Soy Ingeniero electrónico con especialización en desarrollo web, me he enfocado en convertirme en un desarrollador full-stack sólido con gran capacidad para resolver problemas complejos. Inicialmente me formé como ingeniero electrónico, donde adquirí sólidas capacidades analíticas para resolver problemas técnicos. Sin embargo, al final de mi carrera descubrí una pasión por el desarrollo web. Esto me llevó a enfocarme en lenguajes tales como JavaScript, Node.js, Express, bases de datos SQL y NoSQL, así como React para front-end.
+        <Grid xs={5}>
+          
+          <Circuit  className="box-circuit" />  
+            <p>*Al presionar los pulsadores se enciende el led*</p> 
+             
+        </Grid>
 
-Tengo amplia experiencia construyendo aplicaciones web escalables desde cero hasta producción, incluyendo prototipado, diseño de bases de datos optimizadas, desarrollo de API, integraciones con servicios de terceros, optimización y despliegue. Hoy en día estoy especialmente interesado en proyectos de IoT y desarrollo de dispositos basados a los requerimientos segun su necesidad.
+        <Grid xs={7} >
 
-Me considero un ávido resolutor de problemas, constantemente buscando nuevos retos que me obliguen a salir de mi zona de confort y aprender cosas nuevas. Disfruto especialmente descifrando problemas técnicos complejos mediante un enfoque metódico e innovador. Mi sólida formación en ingeniería combinada con mis habilidades de programación web hace que pueda abordar proyectos desafiantes de manera única.
-        </p> 
-    </Grid>
-  </Grid>
+        <p> Soy Ingeniero electrónico con especialización en desarrollo web, me he enfocado en convertirme en un desarrollador full-stack sólido con gran capacidad para resolver problemas complejos. Inicialmente me formé como ingeniero electrónico, donde adquirí sólidas capacidades analíticas para resolver problemas técnicos. Sin embargo, al final de mi carrera descubrí una pasión por el desarrollo web. Esto me llevó a enfocarme en lenguajes tales como JavaScript, Node.js, Express, bases de datos SQL y NoSQL, así como React para front-end.
+
+          Tengo amplia experiencia construyendo aplicaciones web escalables desde cero hasta producción, incluyendo prototipado, diseño de bases de datos optimizadas, desarrollo de API, integraciones con servicios de terceros, optimización y despliegue. Hoy en día estoy especialmente interesado en proyectos de IoT y desarrollo de dispositos basados a los requerimientos segun su necesidad.
+          Me considero un ávido resolutor de problemas, constantemente buscando nuevos retos que me obliguen a salir de mi zona de confort y aprender cosas nuevas. Disfruto especialmente descifrando problemas técnicos complejos mediante un enfoque metódico e innovador. Mi sólida formación en ingeniería combinada con mis habilidades de programación web hace que pueda abordar proyectos desafiantes de manera única.
+        </p>
+
+      </Grid>
+
+</Grid>
+
 
       </Container>
 
